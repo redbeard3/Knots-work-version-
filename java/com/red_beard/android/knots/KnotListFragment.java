ed972bc49344e06016036d107f645b1c35847e0e
red beard <red.beard@bk.ru>
2018-01-16 19:15:15 +0300

    add KnotListFragment

--- java/com/red_beard/android/knots/KnotListFragment.java
+++ java/com/red_beard/android/knots/KnotListFragment.java
@@ -1,108 +1,141 @@
 package com.red_beard.android.knots;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link KnotListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link KnotListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class KnotListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class KnotListFragment extends ListFragment {
 
-    private OnFragmentInteractionListener mListener;
+    CustomCursorAdapter cursorAdapter;
+    private int pos;
+    private KnotDatabaseHelper myDbHelper;
+    private Cursor cursor;
 
-    public KnotListFragment() {
-        // Required empty public constructor
+    static interface KnotListListener{
+        void itemClicked(long id);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment KnotListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static KnotListFragment newInstance(String param1, String param2) {
-        KnotListFragment fragment = new KnotListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private KnotListListener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_knot_list, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        View view = inflater.inflate(R.layout.fragment_knot_list, container, false);
+        return view;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onStart(){
+        super.onStart();
+        pos = (int) getActivity().getIntent().getSerializableExtra(HostActivityTwo.EXTRA_KNOT);     //Получаем, какой тип узлов нужно показать
+        openDB();
+
+        String subtitle = "";
+
+        if (pos == 0) {
+            openKnotList("CLIMB = 1");
+            subtitle = getResources().getString(R.string.climb_top_list);
         }
+
+        if (pos == 1) {
+            openKnotList("SEA = 1");
+            subtitle = getResources().getString(R.string.sea_top_list);
+        }
+
+        if (pos == 2) {
+            openKnotList("FISH = 1");
+            subtitle = getResources().getString(R.string.fish_top_list);
+        }
+
+        if (pos == 3) {
+            openKnotList("OTHER = 1");
+            subtitle = getResources().getString(R.string.mark_top_list);
+        }
+
+        if (pos == 4) {
+            openKnotList("TIE = 1");
+            subtitle = getResources().getString(R.string.tie_top_list);
+        }
+
+        if (pos == 5) {
+            openKnotList("LACE = 1");
+            subtitle = getResources().getString(R.string.lace_top_list);
+        }
+
+        if (pos == 6) {
+            openKnotList("DECOR = 1");
+            subtitle = getResources().getString(R.string.decor_top_list);
+        }
+
+        if (pos == 7) {
+            openKnotList("FAVORITE = 1");
+            subtitle = getResources().getString(R.string.favorite_top_list);
+        }
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setTitle(subtitle);
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onAttach (Activity activity){
+        super.onAttach(activity);
+        this.listener = (KnotListListener) activity;
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        myDbHelper.close();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id){
+        if (listener != null){
+            listener.itemClicked(id);
+        }
+    }
+
+    private void openKnotList(String typeKnotList){
+        try {
+            cursor = myDbHelper.query("KNOT",
+                    new String[] {"_id", "NAME"},
+                    typeKnotList,
+                    null, null, null,
+                    "NAME ASC");
+
+            cursorAdapter = new CustomCursorAdapter(this.getActivity(),
+                    cursor,
+                    0);
+            setListAdapter(cursorAdapter);
+        } catch (SQLException e) {
+            Toast toast = Toast.makeText(this.getActivity(), "Invalid database", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    private void openDB(){
+        myDbHelper = new KnotDatabaseHelper(this.getActivity());
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDbHelper.openDataBase();
+        } catch (SQLException sqle) {
+            throw sqle;
+        }
     }
 }
