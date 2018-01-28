7ba66b33b30ceab478ba0d25fdd661381bf8cba0
red beard <red.beard@bk.ru>
2018-01-28 20:09:31 +0300

    create knots_db.xml

---
+++ java/com/red_beard/android/knots/database_class/DBHelper.java
@@ -0,0 +1,10 @@
+package com.red_beard.android.knots.database_class;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by red beard on 25.01.2018.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+}
