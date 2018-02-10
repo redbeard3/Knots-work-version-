926c6080bb19b73522eda5b7ce6cf9dca3a30c69
red beard <red.beard@bk.ru>
2018-02-10 20:23:16 +0300

    add version name

--- java/com/red_beard/android/knots/SourceActivity.java
+++ java/com/red_beard/android/knots/SourceActivity.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SourceActivity extends AppCompatActivity {
+
+    TextView versionName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.source_activity);
+        versionName = findViewById(R.id.versionName);
+        versionName.setText("Версия " + BuildConfig.VERSION_NAME);
     }
 
     public static Intent newIntent (Context packageContext){
