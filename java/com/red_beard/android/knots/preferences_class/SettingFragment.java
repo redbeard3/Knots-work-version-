29496e19576b482de38951cebc24507f4e924793
red beard <red.beard@bk.ru>
2018-09-27 21:18:19 +0300

    create preferences.xml

---
+++ java/com/red_beard/android/knots/preferences_class/SettingFragment.java
@@ -0,0 +1,10 @@
+package com.red_beard.android.knots.preferences_class;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by red beard on 27.09.2018.
+ */
+
+public class SettingFragment extends PreferenceFragment {
+}
